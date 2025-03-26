aa0d76e8523fda0e5a260c9f7fafd517cc09fd53
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-03-26 12:17:24 -0700

    basic video button and functionality implementation to test if it works on mobile
    author: dangkyle64
    date: 3/26/2025

---
+++ app/_components_/VideoButton.jsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { useRef, useState } from 'react';
+import styles from './VideoButton.module.css';
+
+const VideoButton = () => {
+
+    const videoRef = useRef(null);
+    const stream = useRef(null);
+
+    const [error, setError] = useState(null);
+
+    const startVideo = async () => {
+        try {
+            stream.current = await navigator.mediaDevices.getUserMedia({ video: true });
+            videoRef.current.srcObject = stream.current;
+
+        } catch(error) {
+            console.log(error);
+            setError(`Error accessing camera: ${error}`);
+        };
+    };
+
+    const endVideo = async () => {
+        const tracks = stream.current?.getTracks();
+        tracks?.forEach((tracks) => tracks.stop());
+        videoRef.current.srcObject = null;
+    };
+
+    return (
+        <div>
+            <button id="video-btn" onClick={startVideo}>Start Camera</button>
+            <button id="video-end-btn" onClick={endVideo}>End Camera</button>
+            <video ref={videoRef}></video>
+            {error && <div className={styles.error}>{error}</div>}
+        </div>
+    );
+};
+
+export default VideoButton;
--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
+import VideoButton from "./_components_/VideoButton";
+
 export default async function Home() {
     return (
       <div>
-        <h1>Skeleton Main Page</h1>
+            <h1>Skeleton Main Page</h1>
+            <VideoButton />
       </div>
     );
 };
